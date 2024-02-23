878cb1b16dd3236de7459319f901ebb8a0dcc83b
enhasya <enhasya@outlook.co.id>
2024-02-23 14:56:41 +0700

    update

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,34 +4,34 @@ const Navbar = () => {
   return (
     <nav className="bg-[#161619] flex flex-col lg:flex-row gap-4 lg:gap-0 w-full h-auto p-8 lg:px-6 lg:pt-6 lg:pb-0 items-center justify-between">
       <div className="flex flex-col lg:flex-row w-full lg:w-auto gap-4 lg:gap-0 items-start lg:items-center justify-start">
-        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
+        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full p-0 lg:px-4 lg:py-2 mt-0">
           <p className="text-[#FFFFFF]/80 font-bold text-sm tracking-tight leading-relaxed">
             Componine
           </p>
         </button>
       </div>
       <div className="flex flex-col lg:flex-row w-full lg:w-auto gap-4 lg:gap-0 items-start lg:items-center justify-end">
-        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
+        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full p-0 lg:px-4 lg:py-2 mt-0">
           <p className="text-[#FFFFFF]/80 font-medium text-sm tracking-tight leading-relaxed">
             Components
           </p>
         </button>
-        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
+        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full p-0 lg:px-4 lg:py-2 mt-0">
           <p className="text-[#FFFFFF]/80 font-medium text-sm tracking-tight leading-relaxed">
             Pricing
           </p>
         </button>
-        <label className="bg-transparent hidden lg:flex flex-row gap-2 items-center w-full lg:w-auto rounded-none px-4 py-2 mt-0">
+        <label className="bg-transparent hidden lg:flex flex-row gap-2 items-center w-full lg:w-auto rounded-none p-0 lg:px-4 lg:py-2 mt-0">
           <p className="text-[#FFFFFF]/20 font-medium text-sm tracking-tight leading-relaxed">
             |
           </p>
         </label>
-        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
+        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full p-0 lg:px-4 lg:py-2 mt-0">
           <p className="text-[#FFFFFF]/80 font-medium text-sm tracking-tight leading-relaxed">
             Developer
           </p>
         </button>
-        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
+        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full p-0 lg:px-4 lg:py-2 mt-0">
           <p className="text-[#FFFFFF]/80 font-medium text-sm tracking-tight leading-relaxed">
             Contact
           </p>
--- components/title.jsx
+++ components/title.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import Image from "next/image";
+
+const {
+  default: Documentations,
+} = require("../shared/icons/book-02-white.svg");
+const {
+  default: FindComponents,
+} = require("../shared/icons/compass-white.svg");
 
 const Title = () => {
   return (
@@ -8,18 +16,18 @@ const Title = () => {
         <span className="text-[#E6F40B]">Componine</span>
       </h1>
       <h2 className="text-[#FFFFFF]/80 font-medium text-sm lg:text-lg tracking-tight leading-relaxed lg:leading-relaxed text-start lg:text-center w-full lg:w-[720px] mt-8 lg:mt-16">
-        Designed with versatility and efficiency in mind, "Componine" is more
-        than just another component library. It's a comprehensive toolkit
-        crafted to empower developers like you to create stunning, responsive,
-        and feature-rich UIs effortlessly.
+        Ready to supercharge your development projects? "Componine" is here to
+        revolutionize the way you build dynamic and engaging user interfaces.
       </h2>
       <div className="flex flex-row flex-wrap gap-4 w-full h-auto items-center justify-center mt-8 lg:mt-16">
         <button className="bg-white/10 transition-all hover:bg-white/20 flex flex-row gap-2 items-center w-full lg:w-auto rounded-full px-4 py-2 mt-0">
+          <Image src={Documentations} width="16" height="16" />
           <p className="text-[#FFFFFF]/80 font-medium text-sm tracking-tight leading-relaxed">
             Documentations
           </p>
         </button>
         <button className="bg-white/10 transition-all hover:bg-white/20 flex flex-row gap-2 items-center w-full lg:w-auto rounded-full px-4 py-2 mt-0">
+          <Image src={FindComponents} width="16" height="16" />
           <p className="text-[#FFFFFF]/80 font-medium text-sm tracking-tight leading-relaxed">
             Find Components
           </p>
