1aba3dbfc1c7b14390a24a4b2d7e193c7e298029
enhasya <enhasya@outlook.co.id>
2024-02-23 14:46:18 +0700

    update

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,19 +4,19 @@ const Navbar = () => {
   return (
     <nav className="bg-[#161619] flex flex-col lg:flex-row gap-4 lg:gap-0 w-full h-auto p-8 lg:px-6 lg:pt-6 lg:pb-0 items-center justify-between">
       <div className="flex flex-col lg:flex-row w-full lg:w-auto gap-4 lg:gap-0 items-start lg:items-center justify-start">
-        <button className="bg-transparent transition-all lg:hover:bg-black/20 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
+        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
           <p className="text-[#FFFFFF]/80 font-bold text-sm tracking-tight leading-relaxed">
             Componine
           </p>
         </button>
       </div>
       <div className="flex flex-col lg:flex-row w-full lg:w-auto gap-4 lg:gap-0 items-start lg:items-center justify-end">
-        <button className="bg-transparent transition-all lg:hover:bg-black/20 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
+        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
           <p className="text-[#FFFFFF]/80 font-medium text-sm tracking-tight leading-relaxed">
             Components
           </p>
         </button>
-        <button className="bg-transparent transition-all lg:hover:bg-black/20 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
+        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
           <p className="text-[#FFFFFF]/80 font-medium text-sm tracking-tight leading-relaxed">
             Pricing
           </p>
@@ -26,12 +26,12 @@ const Navbar = () => {
             |
           </p>
         </label>
-        <button className="bg-transparent transition-all lg:hover:bg-black/20 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
+        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
           <p className="text-[#FFFFFF]/80 font-medium text-sm tracking-tight leading-relaxed">
             Developer
           </p>
         </button>
-        <button className="bg-transparent transition-all lg:hover:bg-black/20 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
+        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-white/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
           <p className="text-[#FFFFFF]/80 font-medium text-sm tracking-tight leading-relaxed">
             Contact
           </p>
