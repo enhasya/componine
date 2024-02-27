36e26a46420355604129649a87247bc51cb5fd1c
enhasya <enhasya@outlook.co.id>
2024-02-27 16:57:25 +0700

    update

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,54 +3,53 @@ import Image from "next/image";
 
 const { default: Upcoming } = require("@/shared/icons/wind-01-colorized.svg");
 
+const ContextMessage = {
+  Introduction: "Introducing Componine",
+  Description: "It is an Ode To the Artistry of React.js",
+};
+
 const Navbar = () => {
   return (
     <nav className="bg-[#F8F8FF] lg:bg-transparent lg:hover:bg-opacity-10 border-b-0 lg:border-b lg:border-transparent backdrop-blur-none transition-all lg:hover:border-b lg:hover:border-b-white/10 lg:hover:backdrop-blur-lg flex flex-col lg:flex-row gap-4 lg:gap-0 w-full h-auto px-8 py-8 pb-0 lg:px-32 lg:py-4 items-center justify-between">
       <div className="flex flex-col lg:flex-row w-full lg:w-auto gap-4 lg:gap-2 items-start lg:items-center justify-start">
-        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-black/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full p-0 lg:px-4 lg:py-2 mt-0">
-          <p className="text-[#161619] font-medium text-xs tracking-tight leading-relaxed">
-            Componine
-          </p>
-        </button>
-        <button className="bg-[#F13F0C]/10 ring-1 ring-[#F13F0C] transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-[#F13F0C]/20 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
-          <Image src={Upcoming} width="14" height="14" />
-          <p className="text-[#161619] font-medium text-xs tracking-tight leading-relaxed">
-            <span className="font-bold">Introducing Componine</span> —{" "}
-            <span className="text-[#161619]/80">
-              It is an Ode To the Artistry of React.js
-            </span>
-          </p>
-        </button>
+        <Section name={"Componine"} />
+        <Context />
       </div>
       <div className="flex flex-col lg:flex-row w-full lg:w-auto gap-4 lg:gap-2 items-start lg:items-center justify-end">
-        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-black/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full p-0 lg:px-4 lg:py-2 mt-0">
-          <p className="text-[#161619] font-medium text-xs tracking-tight leading-relaxed">
-            Components
-          </p>
-        </button>
-        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-black/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full p-0 lg:px-4 lg:py-2 mt-0">
-          <p className="text-[#161619] font-medium text-xs tracking-tight leading-relaxed">
-            Pricing
-          </p>
-        </button>
+        <Section name={"Components"} />
+        <Section name={"Pricing"} />
         <label className="bg-transparent hidden lg:flex flex-row gap-2 items-center w-full lg:w-auto rounded-none p-0 mt-0">
           <p className="text-[#161619]/20 font-medium text-xs tracking-tight leading-relaxed">
             |
           </p>
         </label>
-        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-black/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full p-0 lg:px-4 lg:py-2 mt-0">
-          <p className="text-[#161619] font-medium text-xs tracking-tight leading-relaxed">
-            Developer
-          </p>
-        </button>
-        <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-black/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full p-0 lg:px-4 lg:py-2 mt-0">
-          <p className="text-[#161619] font-medium text-xs tracking-tight leading-relaxed">
-            Contact
-          </p>
-        </button>
+        <Section name={"Developer"} />
+        <Section name={"Contact"} />
       </div>
     </nav>
   );
 };
 
 export default Navbar;
+
+const Context = () => {
+  return (
+    <button className="bg-[#F13F0C]/10 ring-1 ring-[#F13F0C] transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-[#F13F0C]/20 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full px-4 py-2 mt-0">
+      <Image src={Upcoming} width="14" height="14" />
+      <p className="text-[#161619] font-medium text-xs tracking-tight leading-relaxed">
+        <span className="font-bold">{ContextMessage.Introduction}</span> —{" "}
+        <span className="text-[#161619]/80">{ContextMessage.Description}</span>
+      </p>
+    </button>
+  );
+};
+
+const Section = ({ name }) => {
+  return (
+    <button className="bg-transparent transition-all hover:opacity-80 lg:hover:opacity-100 lg:hover:bg-black/10 flex flex-row gap-2 items-center w-full lg:w-auto rounded-none lg:rounded-full p-0 lg:px-4 lg:py-2 mt-0">
+      <p className="text-[#161619] font-medium text-xs tracking-tight leading-relaxed">
+        {name}
+      </p>
+    </button>
+  );
+};
